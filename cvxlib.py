da064fa95005ba679fd02aa9f74439587e6fb095
Leonova Anna <aniliar@mail.ru>
2018-04-26 02:20:34 +0400

    added an intersection search for m ellipses

--- cvxlib.py
+++ cvxlib.py
@@ -26,33 +26,65 @@ class Ellipse:
         temp = np.dot(np.dot(self.b.T, np.linalg.matrix_power(self.A, -1)), self.b) 
         self.P = np.dot(np.sqrt(temp - self.c), f_matrix_power(self.A, -0.5))
         self.x_c = np.dot(-np.linalg.matrix_power(self.A, -1), self.b)
-        
-def plotEllipse(ellipses):#don't work
-    p = len(ellipses)
-    i = 0
-    while(i < p):
-        angle = 0.5*np.arctan2(2*ellipses[i].P[0][1],ellipses[i].P[1][1]-ellipses[i].P[0][0])
-        R = np.array([[np.cos(angle), -np.sin(angle)],
-                      [np.sin(angle), np.cos(angle)]])
-        Q = np.dot(np.dot(R, ellipses[i].P), np.transpose(R))
-        print 'angle:', angle*180
-        print 'R:', R
-        print 'P', ellipses[i].P
-        print Q
-        a = plt.subplot(111, aspect='equal')
-        Temp_ellipse = patches.Ellipse(ellipses[i].x_c, 2*np.sqrt(Q[0][0]), 2*np.sqrt(Q[1][1]), -angle*180)
-        print 'a', Q[0][0], 'b', Q[1][1]
-        a.add_artist(Temp_ellipse)
-        Temp_ellipse.set_clip_box(a.bbox)
-        Temp_ellipse.set_alpha(0.1)
-        a.add_artist(Temp_ellipse)
-        i += 1
-    #test = patches.Ellipse([0,0], 1, 0.5, 45)
-    #a.add_artist(test)
-    plt.xlim(-1, 1)
-    plt.ylim(-1.5, 1.5)
-    plt.show()
 
+def distributeSensors(ellipses, alpha):
+    #Set the number of paramers
+    n = 2;
+    #Get the number of targets and sensors
+    m = ellipses.shape[0]
+    p = ellipses.shape[1]
+    #Create resource matrix
+    tau = cvx.Variable(m, p)
+    #Create a variable that is constrained to the positive semidefinite cone.
+    A = []
+    b = [] # Column vector
+    for i in range(m):
+        A.append(cvx.Semidef(n))
+        b.append(cvx.Variable(n))
+        
+    #Set objective
+    objSum = 0
+    for i in range(m):
+        objSum += -cvx.log_det(A[i]) + alpha * cvx.norm(tau[i,:], 1)
+    obj = cvx.Minimize(objSum)
+    
+    #Set constraint
+    constraints = [tau >= 0]
+    for i in range(m):
+        ellipses_sum = np.zeros((2 * n + 1, 2 * n + 1))
+        for j in range(p):
+            #tau(i)*[[ellipses(i).A, ellipses(i).b, np.zeros((n, n))],
+            #        [ellipses(i).b.T, ellipses(i).c, np.zeros((1, n))],
+            #        [np.zeros((n, 2*n+1))]]
+            Matrix_one = np.column_stack((ellipses[i, j].A, ellipses[i, j].b, np.zeros((n, n))))
+            Matrix_two = np.hstack((np.reshape(ellipses[i, j].b, (1, n)), np.reshape(ellipses[i, j].c, (1, 1)), np.reshape(np.zeros(n), (1, n))))
+            Matrix_all = np.vstack((Matrix_one, Matrix_two, np.zeros((n, 2 * n + 1))))
+            ellipses_sum = ellipses_sum + tau[i, j] * Matrix_all
+            
+        #Matrix_final = [[A, b, np.zeros((n, n))],
+        #                [b.T, -1, b.T],
+        #                [np.zeros((n, n)), b, -A]]
+        Matrix_one = cvx.hstack(A[i], b[i], np.zeros((n, n)))
+        Matrix_two = cvx.hstack(b[i].T, -1, b[i].T)
+        Matrix_three = cvx.hstack(np.zeros((n, n)), b[i], -A[i])
+        Matrix_final = cvx.vstack(Matrix_one, Matrix_two, Matrix_three)
+        constraints.append(Matrix_final - ellipses_sum << 0)
+        
+    #Form and solve optimization problem
+    prob = cvx.Problem(obj, constraints)
+    prob.solve(verbose=True, max_iters=1000, refinement = 2)
+    if prob.status != cvx.OPTIMAL:
+        raise Exception('CVXPY Error')
+    
+    #Form output
+    final_ellipses = []
+    for i in range(m):
+        e = Ellipse()
+        temp_c = np.array(np.dot(np.dot(b[i].value.T, np.linalg.matrix_power(A[i].value, -1)), b[i].value) - 1)
+        e.initByCQF(np.array(A[i].value), np.array(b[i].value).reshape(-1), temp_c[0][0])
+        final_ellipses.append(e)
+    return tau.value, final_ellipses
+    
 def findIntersection(ellipses):
     #Set the number of paramers
     n = 2;
@@ -60,7 +92,6 @@ def findIntersection(ellipses):
     p = len(ellipses)
     #Find an intersection:
     #Create a variable that is constrained to the positive semidefinite cone.
-    #A = cvx.semidefinite(n)
     A = cvx.Semidef(n)
     b = cvx.Variable(n) # Column vector
     tau = cvx.Variable(p)
